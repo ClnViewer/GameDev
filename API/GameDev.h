25426587a0f29a1bd2a672417851d098568653e4
admin <admin@pick-nik.ru>
2018-12-25 13:11:33 +0400

    commit by svn repo update, rev: 402

--- API/GameDev.h
+++ API/GameDev.h
@@ -86,8 +86,8 @@ void DLL_EXPORT wndScreenSaverOff(void);
 
 #endif
 /*
-	Last Changed Rev: 396
-	Last Changed Date: 2018-12-23 18:32:47 +0400 (Sun, 23 Dec 2018)
+	Last Changed Rev: 401
+	Last Changed Date: 2018-12-23 18:37:10 +0400 (Sun, 23 Dec 2018)
  */
 
 
