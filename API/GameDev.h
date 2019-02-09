b25ebe565b627b3d5346128c81fa1394280766cb
admin <admin@pick-nik.ru>
2019-02-09 04:24:21 +0400

    --ignore-externals

--- API/GameDev.h
+++ API/GameDev.h
@@ -86,8 +86,8 @@ void DLL_EXPORT wndScreenSaverOff(void);
 
 #endif
 /*
-	Last Changed Rev: 848
-	Last Changed Date: 2019-02-06 05:49:45 +0400 (Wed, 06 Feb 2019)
+	Last Changed Rev: 894
+	Last Changed Date: 2019-02-09 04:24:21 +0400 (Sat, 09 Feb 2019)
  */
 
 
